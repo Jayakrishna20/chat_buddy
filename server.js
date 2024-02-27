3b3c56820fc265db251e2072f447f17b9455509e
Jayakrishna20 <jayakrishnans2041@gmail.com>
2024-02-27 18:05:47 +0530

    client side done

---
+++ server.js
@@ -0,0 +1,18 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json);
+require("dotenv").config();
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEN_AI_KEY);
+
+app.post("/gemini", async (req, res) => {
+  console.log(req.body.history);
+  console.log(req.body.message);
+});
+
+app.listen(PORT, () => console.log(`Listeing on port ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const surpiseOptions = [
+    "Who won the latest Nobel Peace Prize?",
+    "Where does pizza come from?",
+    "Who do you make a BLT sandwich?",
+  ];
+
+  const surprise = () => {
+    const randomValue = surpiseOptions[Math.floor(Math.random() * surpiseOptions.length)];
+    setValue(randomValue);
+  };
+
+  const getResponse = async () => {
+    if (!value) {
+      setError("Error! Please ask a question!");
+      return;
+    }
+
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:8000/gemini", options);
+      const data = await response.text();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setError("Something went wrong! Please try again later.");
+    }
+  };
+
   return (
     <div className="app">
-      <section className="search-section">
-        <p>
-          What do you want to know?
-          <button className="suprise">Surpise me </button>
-        </p>
-      </section>
+      <p>
+        What do you want to know?
+        <button className="surprise" onClick={surprise} disabled={!chatHistory}>
+          Surprise me
+        </button>
+      </p>
+      <div className="input-container">
+        <input
+          value={value}
+          placeholder="When is Onam....?"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {!error && <button onClick={getResponse}>Ask me</button>}
+        {error && <button>Clear</button>}
+      </div>
+      {error && <p>{error}</p>}
+      <div className="search-result">
+        <div key={""}>
+          <p className="answer"></p>
+        </div>
+      </div>
     </div>
   );
 };
